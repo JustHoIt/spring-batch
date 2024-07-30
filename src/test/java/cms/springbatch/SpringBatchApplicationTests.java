567e69cd70ca4aa1b17ed0ef4c25ea4b3f3e4ebf
JustHo <zalop3128@kakao.com>
2024-07-30 16:07:40 +0900

    Initial commit

---
+++ src/test/java/cms/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cms.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

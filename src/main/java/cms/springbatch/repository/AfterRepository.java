118061e6d9772b65cb0409059cc392a6094b4cce
Just_Ho_it <zalop3128@kakao.com>
2024-08-04 21:22:00 +0900

    Batch Class 작성 1. Entity, Repository 파일 작성 2. FirstBatch 파일 작성 (Job, Step, ItemReader, ItemProcessor, ItemWriter)

---
+++ src/main/java/cms/springbatch/repository/AfterRepository.java
@@ -0,0 +1,8 @@
+package cms.springbatch.repository;
+
+import cms.springbatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+
+}
---
+++ src/main/java/cms/springbatch/repository/BeforeRepository.java
@@ -0,0 +1,8 @@
+package cms.springbatch.repository;
+
+import cms.springbatch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+
+}
